f825d19b0edeb4aa673fd917eebc8f30863aaaac
HawksP07 <austinpyzer@gmail.com>
2024-02-20 18:05:29 -0500

    Finished ICE6

--- ICE6/scripts/fetch_script.js
+++ ICE6/scripts/fetch_script.js
@@ -1,16 +1,33 @@
 console.log("fetch_script.js loaded");
 
 // get the button for fetch
+let btn_fetch = $("#fetchJoke");
 
 // create a url variable
+let url_fetch = "https://icanhazdadjoke.com";
 
 //need a header Accept value set to 'application/json'
+let myFetchHeader = {"Accept": "application/json"};
 
 // create the callback for the click
+$(btn_fetch).click(() => {
     // use fetch
+    fetch(url_fetch, {
         // give the necessary header data
-        // first then() to recieve the promise
-            // send the json from the promise on to the next then()
-            // console log the json
-            // set the output
+        headers: myFetchHeader
+    // first then() to recieve the promise
+    }).then((result => {
+        // send the json from the promise on to the next then()
+        return result.json();
+    }))
+    .then((jsonRes) => {
+        // console log the json
+        console.log(jsonRes);
+        // set the output
+        document.getElementById("output").innerText = jsonRes.joke;
+    })
+
+
+});
+
 
--- ICE6/scripts/xhr_script.js
+++ ICE6/scripts/xhr_script.js
@@ -1,19 +1,38 @@
 console.log("xhr_script.js loaded");
 
 // get the button for xhr
+let btn_xhr = $("#xhrJoke");
 
 // create a url variable
+let url_xhr = "https://icanhazdadjoke.com";
 
 // create a click callback that handles the API call
+$(btn_xhr).click(() => {
     // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
     // confiture with open()
+    xhr.open("GET", url_xhr);
     // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
     // create the callback to handle the onreadystatechange
-        // only execute when done and status is 200
-            // get the responseText
-            // console log
-            // JSON.parse the joke
-            // set the output
+    xhr.onreadystatechange = function (){
+            // only execute when done and status is 200
+            if(xhr.status === 200 && xhr.readyState === XMLHttpRequest.DONE){
+                // get the responseText
+                let joke = xhr.responseText;
+                // console log
+                console.log(joke);
+                // JSON.parse the joke
+                var parsedJoke = JSON.parse(joke);
+                // set the output
+                document.getElementById("output").innerText = parsedJoke.joke;
+            }
+            
+
+    }
     // send the request
+    xhr.send();    
+
+});
 
 
