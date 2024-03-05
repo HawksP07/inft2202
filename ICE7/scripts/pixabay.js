4cc04726daf8357c97490d3adf06c51a0f6ba1f2
HawksP07 <austinpyzer@gmail.com>
2024-03-05 18:12:49 -0500

    Finished ICE

--- ICE7/scripts/pixabay.js
+++ ICE7/scripts/pixabay.js
@@ -1,29 +1,52 @@
 /**
- * ADD HEADER COMMENTS WITH YOUR NAME + DATE
+ * Name: Austin Pyzer
+ * Date: March 5. 2024
  */
 console.log('pixabay.js loaded');
 
 // API Key for Pixabay
-const PIXABAY_API_KEY = ''; // normally we would load a key through the .env file
+const PIXABAY_API_KEY = '42719377-a77d49f614ad8c60f33c3640f'; // normally we would load a key through the .env file
 // URL for Pixbay request
 const PIXABAY_URL = 'https://pixabay.com/api/?key=<API_KEY>';
+// https://pixabay.com/api/?key=42719377-a77d49f614ad8c60f33c3640f&page=1&per_page=30
 // Constant for image count
+const IMAGE_COUNT = 30;
 
 
 /**
  * makePosts
  * Creates posts for pictures.
  */
-const makePosts = () => 
+const makePosts = (pictureData) => 
 {
-    // make HTML elements for posts using jQuery, same number as images retrieving
-        // use bootstrap cards and append to
-        // i.e. https://getbootstrap.com/docs/5.2/components/card/#about
-        // create card
-        // card body 
-        // card title
-        // image
-        // card text
+    const blogColumn=  $('.blog-column');
+
+    if(pictureData?.length > 0){
+        for (let i = 0; i < IMAGE_COUNT; i++){
+            let id = i;
+            let pixabayPicture = pictureData[i];
+    
+            let card = $('<div class="card"></div>')
+            .attr("id", "card_" + id)
+            .width(600)
+            .appendTo(blogColumn);
+    
+            let pic = $('<img>')
+                .attr('id', "img-"+i)
+                .attr('src', pixabayPicture.webformatURL)
+                .attr('alt', pixabayPicture.tags)
+                .addClass('card-img-top')
+                .appendTo(card);
+    
+            let cardBody = $('<div class"card-body"></div>')
+            .appendTo(card);
+            // append this to card element
+            let cardParagraph = $('<p class"card-text tags"></p>')
+            .html(pixabayPicture.tags)
+            .appendTo(cardBody);
+            // append to card body
+        }
+    }
 
 };
 
@@ -33,8 +56,26 @@ const makePosts = () =>
  */
 const getPictures = () => 
 {
+    // get images match the following: 30 per page, query search value of "cars", all horizontal only, type photo only
+    const url = `${PIXABAY_URL.replace('<API_KEY>', PIXABAY_API_KEY)}&q=cars&orientation=horizontal&image_type=photo&per_page=${IMAGE_COUNT}`;
     // use fetch to get the pictures from the API
+    fetch(url)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        if(data?.hits?.length > 0){
+            makePosts(data?.hits);
+            console.log(data?.hits);
+        }
+    })
     // handle error(s) with .catch()
+    .catch((err) => {
+        //log to console
+        console.log(err);
+    })
+
 };
 
-makePosts();
+getPictures();
